9621ffd3ed950e1b68961fba8afd14ec4f5ffec4
lorsanta <lorenzo.santangelo998@gmail.com>
2024-07-22 22:37:07 +0200

    changed hash, add mandatory files for br2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 85c2685ffc016d009435158b3adcdf436aef39cd
+AESD_ASSIGNMENTS_VERSION = 52774fb4d27dce6a8dff187bb79ab00ee097d9dd
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
